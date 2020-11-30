f6b055c0281bf60b6edab49fcb3ea95233240512
truong-dung <72637123+truong-dung@users.noreply.github.com>
2020-11-30 22:18:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bài_thực_hành_số_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
